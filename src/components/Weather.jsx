9b2992b2c6001b4280267867acd0ecaf0da07654
bidanee <dleksql119@gmail.com>
2023-11-22 19:53:05 +0900

    data success

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,7 @@
-import { FaSearchLocation } from "react-icons/fa";
-import { getWeatherData } from "./api";
+import { PiSunBold } from "react-icons/pi";
+import { getNowWeatherData } from "./api";
 
-export default function Weather() {
+export default function TodayWeather() {
   const today = new Date();
   const date = () => {
     if (today.getHours() === 0) {
@@ -12,17 +12,66 @@ export default function Weather() {
       return `${today.getFullYear()}${today.getMonth() + 1}${today.getDate()}`;
     }
   };
-
-  const time = () => {
+  const fcstTime = () => {
+    if (today.getHours() === 0) return "2400";
     if (today.getHours().length === 1) return `0${today.getHours()}00`;
     return `${today.getHours()}00`;
   };
 
-  console.log(getWeatherData(date(), time()));
+  const nowTime = today.getHours();
+  const time = () => {
+    switch (nowTime) {
+      case 2:
+      case 3:
+      case 4:
+        return "0200";
+      case 5:
+      case 6:
+      case 7:
+        return "0500";
+      case 8:
+      case 9:
+      case 10:
+        return "0800";
+      case 11:
+      case 12:
+      case 13:
+        return "1100";
+      case 14:
+      case 15:
+      case 16:
+        return "1400";
+      case 17:
+      case 18:
+      case 19:
+        return "1700";
+      case 20:
+      case 21:
+      case 22:
+        return "2000";
+      case 23:
+      case 0:
+      case 1:
+        return "2300";
+    }
+  };
 
+  const data = getNowWeatherData(date(), time(), "61", "126").then((result) => {
+    const dataSet = result.filter((x) => x.fcstTime === fcstTime());
+    return dataSet;
+  });
+  console.log(data);
   return (
-    <button className="border-black btn btn-sm btn-square btn-outline btn-info">
-      <FaSearchLocation className="text-xl leading-6 text-black cursor-pointer" />
-    </button>
+    <div className="flex m-4 justify-center">
+      <div className="flex justify-center items-center mr-4 p-2">
+        <PiSunBold size="8rem" />
+      </div>
+      <div className="flex flex-col">
+        <span className="text-xl mb-2 ">맑음</span>
+        <span className="text-xl mb-2 ">현재 기온 10°</span>
+        <span className="text-xl mb-2 ">강수 확률</span>
+        <span className="text-xl ">습도</span>
+      </div>
+    </div>
   );
 }
